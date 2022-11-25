4aca5a2731ef9b162babb4b684140c99f46348e0
Renato Alves Felix <rfelix@redhat.com>
2022-11-25 13:57:05 -0300

    wip

--- scripts/a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Script created to invoke jmeter test script with the slave POD IP addresses
-#Script should be run like: ./load_test "path to the test script in jmx format"
-
-
-export x="/tmp/test-jms.jmx"
-name=$(basename "$x" ".jmx")
-
-echo "--> $name"
